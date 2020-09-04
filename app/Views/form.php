93d63a23f0f26157124a12e4c2b6f0da70cf8433
Biplav Subedi <biplav.subedi@incsub.com>
2020-09-04 17:37:08 +0545

    [Fix] il18n method

--- app/Views/form.php
+++ app/Views/form.php
@@ -16,11 +16,11 @@
 
 				<div class="sui-box-header">
 					<h2 class="sui-box-title">
-					<?php esc_html_e( 'Front End Submission', 'fe-submission' ); ?></h2>
+					<?php echo esc_html__( 'Front End Submission', 'fe-submission' ); ?></h2>
 				</div>
 
 				<div class="sui-box-body">
-					<p><?php esc_html_e( 'Submit Guest posts.', 'fe-submission' ); ?></p>
+					<p><?php echo esc_html__( 'Submit Guest posts.', 'fe-submission' ); ?></p>
 				</div>
 
 			</div>
@@ -34,10 +34,10 @@
 						<!-- Post Title -->
 						<div class="sui-form-field">
 
-							<label for="fe-post-title" id="fe-post-title" class="sui-label"><?php esc_html_e( 'Post Title', 'fe-submission' ); ?></label>
+							<label for="fe-post-title" id="fe-post-title" class="sui-label"><?php echo esc_html__( 'Post Title', 'fe-submission' ); ?></label>
 							<input
 								type="text"
-								placeholder="<?php esc_html_e( 'Enter post title', 'fe-submission' ); ?>"
+								placeholder="<?php echo esc_html__( 'Enter post title', 'fe-submission' ); ?>"
 								id="fe-post-title"
 								class="sui-form-control fe-post-title"
 								name="fe-post-title"
@@ -51,7 +51,7 @@
 						<!-- Post Type -->
 						<div class="sui-form-field">
 
-							<label for="fe-post-type" id="fe-post-type-label" class="sui-label"><?php esc_html_e( 'Post Type', 'fe-submission' ); ?></label>
+							<label for="fe-post-type" id="fe-post-type-label" class="sui-label"><?php echo esc_html__( 'Post Type', 'fe-submission' ); ?></label>
 							<select id="fe-post-type" name="fe-post-type" aria-labelledby="fe-post-type-label" class="sui-select fe-post-type">
 								<?php
 								$all_post_types = get_post_types( array( 'public' => true ), 'objects' ); //only get public posttypes for now.
@@ -73,7 +73,7 @@
 						<!-- Post Content -->
 						<div class="sui-form-field">
 
-							<label class="sui-label sui-label-editor" for="fe-post-content"><?php esc_html_e( 'Post content', 'fe-submission' ); ?></label>
+							<label class="sui-label sui-label-editor" for="fe-post-content"><?php echo esc_html__( 'Post content', 'fe-submission' ); ?></label>
 							<?php wp_editor(
 								'',
 								'fe-post-content',
@@ -91,9 +91,9 @@
 						<!-- Post Excerpt -->
 						<div class="sui-form-field">
 
-							<label id="fe-post-excerpt-label" class="sui-label sui-label-editor" for="fe-post-content"><?php esc_html_e( 'Post Excerpt', 'fe-submission' ); ?></label>
+							<label id="fe-post-excerpt-label" class="sui-label sui-label-editor" for="fe-post-content"><?php echo esc_html__( 'Post Excerpt', 'fe-submission' ); ?></label>
 							<textarea
-								placeholder="<?php esc_html_e( 'Enter post excerpt', 'fe-submission' ); ?>"
+								placeholder="<?php echo esc_html__( 'Enter post excerpt', 'fe-submission' ); ?>"
 								id="fe-post-excerpt"
 								name="fe-post-excerpt"
 								class="sui-form-control fe-post-excerpt"
@@ -108,15 +108,15 @@
 						<!-- Post Image -->
 						<div class="sui-form-field">
 
-							<label class="sui-label"><?php esc_html_e( 'Post Image', 'fe-submission' ); ?></label>
+							<label class="sui-label"><?php echo esc_html__( 'Post Image', 'fe-submission' ); ?></label>
 
 							<div class="avatar-upload">
 								<div class="avatar-edit">
-									<input 
+									<input
 									type="file"
 									name="fe-post-image"
 									id="fe-post-image"
-									accept=".png, .jpg, .jpeg" 
+									accept=".png, .jpg, .jpeg"
 									class="fe-post-image"
 									/>
 									<label for="fe-post-image"></label>
@@ -141,11 +141,11 @@
 
 								<span class="sui-loading-text">
 									<i class="sui-icon-save" aria-hidden="true"></i>
-									<?php esc_html_e( 'Submit Post', 'fe-submission' ); ?>
+									<?php echo esc_html__( 'Submit Post', 'fe-submission' ); ?>
 									<i class="sui-loader" aria-hidden="true"></i>
 								</span>
 							</button>
-							
+
 
 						</div>
 
@@ -163,13 +163,13 @@
 
 				<div class="sui-message-content">
 
-					<p><?php esc_html_e( 'Thank you for submitting a post. Your post will now be reviewed for approval', 'fe-submission' ); ?></p>
+					<p><?php echo esc_html__( 'Thank you for submitting a post. Your post will now be reviewed for approval', 'fe-submission' ); ?></p>
 
 					<!--
 					NOTE:
 					Always add button(s) inside paragraph tag.
 					-->
-					<p><a href="." class="button sui-button"><?php esc_html_e( 'Submit another post', 'fe-submission' ); ?> </a></p>
+					<p><a href="." class="button sui-button"><?php echo esc_html__( 'Submit another post', 'fe-submission' ); ?> </a></p>
 
 				</div>
 
